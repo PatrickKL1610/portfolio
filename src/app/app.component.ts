7ff2d83ddd5c30119fc2c0bdef164fed2acad518
Patrick Klein <patrickklein611@gmail.com>
2024-06-05 16:45:39 +0200

    add start component add colors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { StartComponent } from './start/start.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, StartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
