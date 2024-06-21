5c2e07556f4787c213ba266ec6c64a7c134518a0
Patrick Klein <patrickklein611@gmail.com>
2024-06-21 18:24:13 +0200

    add about me

---
+++ src/app/aboutme/aboutme.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aboutme',
+  standalone: true,
+  imports: [],
+  templateUrl: './aboutme.component.html',
+  styleUrl: './aboutme.component.scss',
+})
+export class AboutmeComponent {}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,25 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { StartComponent } from './start/start.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, StartComponent],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    NavbarComponent,
+    StartComponent,
+    AboutmeComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'portfolio';
+  constructor() {
+    AOS.init();
+  }
 }
