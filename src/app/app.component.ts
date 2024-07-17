1e4eeba8b4b0c9e7cdcebd009f4a6d0f8ad5189c
Patrick Klein <patrickklein611@gmail.com>
2024-07-17 15:24:10 +0200

    bug fixes | add contact component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,13 @@ import { AboutmeComponent } from './aboutme/aboutme.component';
 import AOS from 'aos';
 import { SkillsComponent } from './skills/skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
   imports: [
     CommonModule,
     RouterOutlet,
@@ -19,9 +22,8 @@ import { PortfolioComponent } from './portfolio/portfolio.component';
     AboutmeComponent,
     SkillsComponent,
     PortfolioComponent,
+    ContactComponent,
   ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'portfolio';
