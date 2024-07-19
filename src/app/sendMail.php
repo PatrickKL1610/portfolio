bad9ed633f8093c153fd1c3d7a5beb67ffb5a0b4
Patrick Klein <patrickklein611@gmail.com>
2024-07-19 16:47:14 +0200

    fix sendmail.php

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -27,7 +27,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $headers[] = 'Content-type: text/html; charset=utf-8';
 
             // Additional headers
-            $headers[] = "From: $email";
+            $headers[] = "From: noreply@klein-patrick.ch";
 
             mail($recipient, $subject, $message, implode("\r\n", $headers));
             break;
