6e5b21a25865dfdd74f16ff78a50b13f5106527d
Patrick Klein <patrickklein611@gmail.com>
2024-07-19 17:54:35 +0200

    add impress navigation to home

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [],
+  imports: [RouterModule, FooterComponent],
   templateUrl: './imprint.component.html',
-  styleUrl: './imprint.component.scss'
+  styleUrl: './imprint.component.scss',
 })
 export class ImprintComponent {
+  constructor(private router: Router) {}
 
+  navigateHome() {
+    this.router.navigate(['/']);
+  }
 }
