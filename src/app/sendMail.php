4ae54b44fa761c61f205bd5cacd3f5aedd3f328f
Patrick Klein <patrickklein611@gmail.com>
2024-07-22 19:13:23 +0200

    add about me text

--- src/app/sendMail.php
+++ src/app/sendMail.php
@@ -27,7 +27,7 @@ switch ($_SERVER['REQUEST_METHOD']) {
             $headers[] = 'Content-type: text/html; charset=utf-8';
 
             // Additional headers
-            $headers[] = "From: noreply@klein-patrick.ch";
+            $headers[] = "From: <$email>";
 
             mail($recipient, $subject, $message, implode("\r\n", $headers));
             break;
